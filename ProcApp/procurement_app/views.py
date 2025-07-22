6409cc87c65873ec14a28ab5de735adcdb73cab1
Jim Kennedy <kennedyomata@gmail.com>
2025-07-22 09:12:22 +0300

    added pagination and new route to aie/detail

--- ProcApp/procurement_app/views.py
+++ ProcApp/procurement_app/views.py
@@ -1,3 +1,4 @@
+
 # procurement_app/views.py
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 
@@ -11,6 +12,37 @@ def create_aie():
         flash('AIE form submitted (processing not implemented yet)!', 'info')
         return redirect(url_for('app.create_aie')) # Redirect back or to a success page
     return render_template('aie/create_aie.html')
+    # return render_template('base.html')
+
+# Route to list all AIE requests
+@app_bp.route('/aie/list', methods=['GET', 'POST'])
+def list_aie():
+    # In a real app, fetch AIEs and pagination from the database
+    class Pagination:
+        def __init__(self):
+            self.first_item = 1
+            self.last_item = 1
+            self.total_items = 1
+            self.page = 1
+            self.has_prev = False
+            self.has_next = False
+            self.prev_num = 1
+            self.next_num = 1
+        def iter_pages(self):
+            return [1]
+
+    pagination = Pagination()
+    aies = []  # Empty list for now
+    if request.method == 'POST':
+        flash('AIE form submitted (processing not implemented yet)!', 'info')
+        return redirect(url_for('app.list_aie'))  # Redirect after POST
+    return render_template('aie/aie_list.html', aies=aies, pagination=pagination)
+
+# Route to show details for a specific AIE request
+@app_bp.route('/aie/detail', methods=['GET'])
+def aie_detail():
+    # In a real app, fetch AIE details from the database
+    return render_template('aie/aie_detail.html')
 
 # Other routes for your application phases go here
 @app_bp.route('/aie/approvals', methods=['GET'])
